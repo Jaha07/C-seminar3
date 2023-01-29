6023d7c5ea0b67d7934f359b3acbce4e2591cdcb
Jaha <zlgsbazarbaev@gmail.com>
2023-01-29 20:58:41 +0500

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №17. Напишите программу, которая
+// принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти
+// плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+Console.Write("Enter X: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (y > 0)
+{
+    if (x > 0)
+    {
+        Console.WriteLine("First period");
+    }
+    else
+    {
+        Console.WriteLine("Second period");
+    }
+}
+else
+{
+    if (x > 0)
+    {
+        Console.WriteLine("Fourth period");
+    }
+    else
+    {
+        Console.WriteLine("Third period");
+    }
+
+}
