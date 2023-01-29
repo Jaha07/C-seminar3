40e65567cdc0e8a58c399a97c7d3f3e9ce682bea
Jaha <zlgsbazarbaev@gmail.com>
2023-01-29 22:27:22 +0500

    added task4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №22. Работа в группах
+// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N
+
+
+Console.Clear();
+Console.Write("Enter any number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+// Console.Write($"{Math.Pow(count, 2)}");
+// count++;
+
+while (count <= number)
+{
+    // double result = Math.Pow(count++, 2);
+    Console.Write($"{Math.Pow(count, 2)}");
+    if (count != number) Console.Write(", ");
+    count++;
+}
